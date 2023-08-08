a38666f5a7e25c0275d2644bcb47814a822ae985
Filip <filipfajfer5@gmail.com>
2023-08-08 21:01:04 +0200

    Fix display operator

--- Poly.cpp
+++ Poly.cpp
@@ -12,25 +12,13 @@ std::ostream& operator<<(std::ostream& os, const Poly& p)
     for (auto term = p.terms.rbegin(); term != p.terms.rend(); term++) {
         int order = term->first;
         double coefficient = term->second;
-        if (isFirst) {
-            os << coefficient << "x^" << order;
-            isFirst = false;
-        }
-        else {
-            char sign = 43 + 2*(coefficient < 0);
-            if (order > 1) {
-                os << " " << sign << " " << fabs(coefficient) << "x^" << order;
-            }
-            else if (order == 1) {
-                os << " " << sign << " " << fabs(coefficient) << "x";
-            }
-            else {
-                os << " " << sign << " " << fabs(coefficient);
-            }
-        }
-    }
-    if (isFirst) {
-        os << 0;
+        char sign = 43 + 2*(coefficient < 0);
+        (isFirst && coefficient < 0) ? os << sign : os << "";
+        (!isFirst) ? os << " " << sign << " " : os << "";
+        (fabs(coefficient) != 1 || order == 0) ? os << fabs(coefficient) : os << "";
+        (order > 0) ? os << "x" : os << "";
+        (order > 1) ? os << "^" << order : os << "";
+        isFirst = false;
     }
     return os;
 }
